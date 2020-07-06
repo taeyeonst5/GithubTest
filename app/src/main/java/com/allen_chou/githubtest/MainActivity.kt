26d48248388d2c7ded6ba3acce67be31fcfeb890
allen.chou <allen.chou@ICANMACdeMacBook-Pro.local>
2020-07-06 20:54:54 +0800

    project init

---
+++ app/src/main/java/com/allen_chou/githubtest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.allen_chou.githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.allen_chou.githubtest.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/allen_chou/githubtest/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.allen_chou.githubtest.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
